0f7633dcc6219fd06f69b4fa2c383ea3a62371c8
joao <joao.simsic@4x4brasil.com.br>
2026-01-19 22:14:20 -0300

    refac: moved validation to request

--- src/main/java/com/example/message/core/domain/User.java
+++ src/main/java/com/example/message/core/domain/User.java
@@ -1,8 +1,5 @@
 package com.example.message.core.domain;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
   private Long id;
-
-  @NotBlank(message = "Name cannot be empty")
-  @Size(min = 4, max = 50, message = "Name must be between 4 and 50 characters")
   private String name;
-
-  @NotBlank(message = "Email cannot be empty")
-  @Email(message = "Invalid email format")
   private String email;
 }
