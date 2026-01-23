fe1cbde2ced1e22fa5b1efeaaa690318b1baf8ec
Joao Pedro Simsic <joao.simsic@4x4brasil.com.br>
2026-01-23 15:11:30 -0300

    chore: rename beam file

--- src/main/java/com/example/message/infrastructure/config/BeanConfig.java
+++ src/main/java/com/example/message/infrastructure/config/BeanConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfiguration {
+public class BeanConfig {
   @Bean
   public UserUseCase userUseCase(UserRepositoryPort userRepositoryPort) {
     return new UserService(userRepositoryPort);
