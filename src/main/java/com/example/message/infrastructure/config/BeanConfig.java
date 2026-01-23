97405314b1619172f4247d596a1641387af686ad
Joao Pedro Simsic <joao.simsic@4x4brasil.com.br>
2026-01-23 17:22:15 -0300

    hotfix: minimum secret size and bean config

--- src/main/java/com/example/message/infrastructure/config/BeanConfig.java
+++ src/main/java/com/example/message/infrastructure/config/BeanConfig.java
@@ -5,11 +5,12 @@ import com.example.message.core.ports.output.UserRepositoryPort;
 import com.example.message.core.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
   @Bean
-  public UserUseCase userUseCase(UserRepositoryPort userRepositoryPort) {
-    return new UserService(userRepositoryPort);
+  public UserUseCase userUseCase(UserRepositoryPort userRepositoryPort, PasswordEncoder passwordEncoder) {
+    return new UserService(userRepositoryPort, passwordEncoder);
   }
 }
