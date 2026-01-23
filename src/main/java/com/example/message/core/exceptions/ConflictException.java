fd0120f8221777797244d6a9c614a6090d163bb2
Joao Pedro Simsic <joao.simsic@4x4brasil.com.br>
2026-01-23 14:03:12 -0300

    feat: business exception

---
+++ src/main/java/com/example/message/core/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.example.message.core.exceptions;
+
+public class ConflictException extends BusinessException {
+  public ConflictException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/example/message/core/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package com.example.message.core.exceptions;
+
+public class ValidationException extends BusinessException {
+  public ValidationException(String message) {
+    super(message);
+  }
+}
