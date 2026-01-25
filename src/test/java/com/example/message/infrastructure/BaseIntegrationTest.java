6aef2a72d198baa529387dc513f8a681af129f07
joao <joao.simsic@4x4brasil.com.br>
2026-01-25 15:34:48 -0300

    feat: base integration test abstract

---
+++ src/test/java/com/example/message/infrastructure/BaseIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.example.message.infrastructure;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+  @LocalServerPort protected Integer port;
+
+  @Container
+  @SuppressWarnings("resource")
+  static final PostgreSQLContainer<?> postgres =
+      new PostgreSQLContainer<>("postgres:15-alpine")
+          .withDatabaseName("db")
+          .withUsername("test")
+          .withPassword("test");
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+
+    registry.add("spring.flyway.url", postgres::getJdbcUrl);
+    registry.add("spring.flyway.user", postgres::getUsername);
+    registry.add("spring.flyway.password", postgres::getPassword);
+  }
+
+  @BeforeEach
+  void setUpBase() {
+    RestAssured.baseURI = "http://localhost";
+    RestAssured.port = port;
+  }
+}
