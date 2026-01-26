f4d3df52fef92c40171ada28c520602443fc7a84
joao <joao.simsic@4x4brasil.com.br>
2026-01-25 22:26:20 -0300

    fix: unit tests

--- src/test/java/com/example/message/MessageApplicationTests.java
+++ src/test/java/com/example/message/MessageApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.message;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.message.infrastructure.BaseIntegrationTest;
 
-@SpringBootTest
 class MessageApplicationTests extends BaseIntegrationTest {
 
 	@Test
