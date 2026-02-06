4e7adbf84580d094611718ea8d3fb5865e11f5c7
joao <joao.simsic@4x4brasil.com.br>
2026-02-05 21:25:44 -0300

    feat: rabbitmq adapter

---
+++ src/main/java/com/example/message/infrastructure/adapters/output/messaging/RabbitMQAdapter.java
@@ -0,0 +1,19 @@
+package com.example.message.infrastructure.adapters.output.messaging;
+
+import com.example.message.core.events.UserCreatedEvent;
+import com.example.message.core.ports.output.MessagePublisherPort;
+import com.example.message.infrastructure.config.RabbitConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev")
+public class RabbitMQAdapter implements MessagePublisherPort {
+  private final RabbitTemplate rabbitTemplate;
+
+  @Override
+  public void publish(UserCreatedEvent event) {
+    rabbitTemplate.convertAndSend(
+        RabbitConfig.USER_EXCHANGE, RabbitConfig.USER_CREATED_ROUTING_QUEUE, event);
+  }
+}
