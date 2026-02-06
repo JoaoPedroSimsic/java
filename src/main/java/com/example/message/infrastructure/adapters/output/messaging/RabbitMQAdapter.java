ecf69e7dc8f2ed821c3baf5f30125c262bf4ffa3
joao <joao.simsic@4x4brasil.com.br>
2026-02-05 22:28:46 -0300

    fix: adapters import

--- src/main/java/com/example/message/infrastructure/adapters/output/messaging/RabbitMQAdapter.java
+++ src/main/java/com/example/message/infrastructure/adapters/output/messaging/RabbitMQAdapter.java
@@ -4,12 +4,17 @@ import com.example.message.core.events.UserCreatedEvent;
 import com.example.message.core.ports.output.MessagePublisherPort;
 import com.example.message.infrastructure.config.RabbitConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("dev")
 public class RabbitMQAdapter implements MessagePublisherPort {
   private final RabbitTemplate rabbitTemplate;
+
+  public RabbitMQAdapter(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
 
   @Override
   public void publish(UserCreatedEvent event) {
