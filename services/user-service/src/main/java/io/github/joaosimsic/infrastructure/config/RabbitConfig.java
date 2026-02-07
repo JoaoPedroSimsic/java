ff5aa352733c1f16965b080bd4ab36e01e939696
joao <joao.simsic@4x4brasil.com.br>
2026-02-07 01:58:39 -0300

    refac: rabbitmq config

--- services/user-service/src/main/java/io/github/joaosimsic/infrastructure/config/RabbitConfig.java
+++ services/user-service/src/main/java/io/github/joaosimsic/infrastructure/config/RabbitConfig.java
@@ -10,8 +10,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
   public static final String USER_EXCHANGE = "user.exchange";
+
   public static final String USER_CREATED_QUEUE = "user.created.queue";
-  public static final String USER_CREATED_ROUTING_QUEUE = "user.created";
+
+  public static final String USER_UPDATED_QUEUE = "user.updated.queue";
+
+  public static final String USER_DELETED_QUEUE = "user.deleted.queue";
+
+  public static final String USER_CREATED_ROUTING_KEY = "user.created";
+
+  public static final String USER_UPDATED_ROUTING_KEY = "user.updated";
+
+  public static final String USER_DELETED_ROUTING_KEY = "user.deleted";
 
   @Bean
   public TopicExchange userExchange() {
@@ -25,6 +35,26 @@ public class RabbitConfig {
 
   @Bean
   public Binding userCreatedBinding(TopicExchange userExchange, Queue userCreatedQueue) {
-    return BindingBuilder.bind(userCreatedQueue).to(userExchange).with(USER_CREATED_ROUTING_QUEUE);
+    return BindingBuilder.bind(userCreatedQueue).to(userExchange).with(USER_CREATED_ROUTING_KEY);
+  }
+
+  @Bean
+  public Queue userUpdatedQueue() {
+    return new Queue(USER_UPDATED_QUEUE);
+  }
+
+  @Bean
+  public Binding userUpdatedBinding(TopicExchange userExchange, Queue userUpdatedQueue) {
+    return BindingBuilder.bind(userUpdatedQueue).to(userExchange).with(USER_UPDATED_ROUTING_KEY);
+  }
+
+  @Bean
+  public Queue userDeletedQueue() {
+    return new Queue(USER_DELETED_QUEUE);
+  }
+
+  @Bean
+  public Binding userDeletedBinding(TopicExchange userExchange, Queue userDeletedQueue) {
+    return BindingBuilder.bind(userDeletedQueue).to(userExchange).with(USER_DELETED_ROUTING_KEY);
   }
 }
