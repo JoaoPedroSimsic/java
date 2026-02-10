f133eda56f059679c61a2f3d40727f9a9ff8828b
joao <joao.simsic@4x4brasil.com.br>
2026-02-10 12:38:03 -0300

    refac: remove token port

--- services/user-service/src/main/java/io/github/joaosimsic/core/ports/output/TokenPort.java
+++
@@ -1,9 +0,0 @@
-package io.github.joaosimsic.core.ports.output;
-
-import io.github.joaosimsic.core.domain.User;
-
-public interface TokenPort {
-  String generateToken(User user);
-
-  String validateToken(String token);
-}
