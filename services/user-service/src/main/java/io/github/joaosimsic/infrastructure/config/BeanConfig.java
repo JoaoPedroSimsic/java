1b965b89ce4047c69fd224d9c04de8a4fd827a0a
joao <joao.simsic@4x4brasil.com.br>
2026-02-10 17:26:31 -0300

    fix: removed passwd encryption on bean config

--- services/user-service/src/main/java/io/github/joaosimsic/infrastructure/config/BeanConfig.java
+++ services/user-service/src/main/java/io/github/joaosimsic/infrastructure/config/BeanConfig.java
@@ -6,13 +6,11 @@ import io.github.joaosimsic.core.ports.output.UserPort;
 import io.github.joaosimsic.core.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
   @Bean
-  UserUseCase userUseCase(
-      UserPort userRepositoryPort, PasswordEncoder passwordEncoder, OutboxPort outboxPort) {
-    return new UserService(userRepositoryPort, passwordEncoder, outboxPort);
+  UserUseCase userUseCase(UserPort userRepositoryPort, OutboxPort outboxPort) {
+    return new UserService(userRepositoryPort, outboxPort);
   }
 }
