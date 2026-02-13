6cd78bf9e836852da62e94f3ddea3605834b6ae4
joao <joao.simsic@4x4brasil.com.br>
2026-02-13 17:50:33 -0300

    refac: rabbit config

--- services/user-service/src/main/java/io/github/joaosimsic/infrastructure/config/RabbitConfig.java
+++ services/user-service/src/main/java/io/github/joaosimsic/infrastructure/config/RabbitConfig.java
@@ -1,27 +1,34 @@
 package io.github.joaosimsic.infrastructure.config;
 
+import io.github.joaosimsic.core.events.AuthUserRegisteredEvent;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RabbitConfig {
   public static final String USER_EXCHANGE = "user.exchange";
+  public static final String AUTH_EXCHANGE = "auth.exchange";
 
   public static final String USER_CREATED_QUEUE = "user.created.queue";
-
   public static final String USER_UPDATED_QUEUE = "user.updated.queue";
-
   public static final String USER_DELETED_QUEUE = "user.deleted.queue";
+  public static final String AUTH_USER_REGISTERED_QUEUE = "auth.user.registered.queue";
 
   public static final String USER_CREATED_ROUTING_KEY = "user.created";
-
   public static final String USER_UPDATED_ROUTING_KEY = "user.updated";
-
   public static final String USER_DELETED_ROUTING_KEY = "user.deleted";
+  public static final String AUTH_USER_REGISTERED_ROUTING_KEY = "auth.user.registered";
 
   @Bean
   TopicExchange userExchange() {
@@ -56,5 +63,43 @@ public class RabbitConfig {
   @Bean
   Binding userDeletedBinding(TopicExchange userExchange, Queue userDeletedQueue) {
     return BindingBuilder.bind(userDeletedQueue).to(userExchange).with(USER_DELETED_ROUTING_KEY);
+  }
+
+  @Bean
+  TopicExchange authExchange() {
+    return new TopicExchange(AUTH_EXCHANGE);
+  }
+
+  @Bean
+  Queue authUserRegisteredQueue() {
+    return new Queue(AUTH_USER_REGISTERED_QUEUE);
+  }
+
+  @Bean
+  Binding authUserRegisteredBinding(TopicExchange authExchange, Queue authUserRegisteredQueue) {
+    return BindingBuilder.bind(authUserRegisteredQueue).to(authExchange).with(AUTH_USER_REGISTERED_ROUTING_KEY);
+  }
+
+  @Bean
+  Jackson2JsonMessageConverter messageConverter() {
+    Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+    DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+    
+    Map<String, Class<?>> idClassMapping = new HashMap<>();
+    idClassMapping.put("io.github.joaosimsic.core.events.UserRegisteredEvent", AuthUserRegisteredEvent.class);
+    typeMapper.setIdClassMapping(idClassMapping);
+    typeMapper.setTrustedPackages("io.github.joaosimsic.core.events");
+    
+    converter.setJavaTypeMapper(typeMapper);
+    return converter;
+  }
+
+  @Bean
+  SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+      ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setMessageConverter(messageConverter);
+    return factory;
   }
 }
