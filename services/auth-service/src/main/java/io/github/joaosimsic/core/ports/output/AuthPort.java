6bbeaf58f6e317e49b9666ee9cb9afcd8d1455e6
joao <joao.simsic@4x4brasil.com.br>
2026-02-12 22:53:18 -0300

    feat: auth port

---
+++ services/auth-service/src/main/java/io/github/joaosimsic/core/ports/output/AuthPort.java
@@ -0,0 +1,20 @@
+package io.github.joaosimsic.core.ports.output;
+
+import io.github.joaosimsic.core.domain.AuthTokens;
+import io.github.joaosimsic.core.domain.AuthUser;
+
+public interface AuthPort {
+  AuthUser createUser(String name, String email, String password);
+
+  AuthTokens login(String email, String password);
+
+  AuthTokens refreshToken(String refreshToken);
+
+  void logout(String accessToken);
+
+  String getGitHubAuthUrl(String redirectUri, String state);
+
+  AuthTokens exchangeCodeForTokens(String code, String redirectUri);
+
+  AuthUser getUserInfo(String accessToken);
+}
