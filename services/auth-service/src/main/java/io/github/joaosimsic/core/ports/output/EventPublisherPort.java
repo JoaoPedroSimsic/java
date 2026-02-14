88e8f50a349fb45966b70e43e6a0177279659cdd
joao <joao.simsic@4x4brasil.com.br>
2026-02-14 00:45:13 -0300

    feat: email update on event publisher

--- services/auth-service/src/main/java/io/github/joaosimsic/core/ports/output/EventPublisherPort.java
+++ services/auth-service/src/main/java/io/github/joaosimsic/core/ports/output/EventPublisherPort.java
@@ -1,7 +1,10 @@
 package io.github.joaosimsic.core.ports.output;
 
+import io.github.joaosimsic.core.events.UserEmailUpdatedEvent;
 import io.github.joaosimsic.core.events.UserRegisteredEvent;
 
 public interface EventPublisherPort {
   void publishUserRegistered(UserRegisteredEvent event);
+
+  void publishUserEmailUpdated(UserEmailUpdatedEvent event);
 }
