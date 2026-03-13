98f8e61c07acb4532f7f7eeb4cc8818e069c4c67
Joao Pedro Simsic <joao.simsic@4x4brasil.com.br>
2026-03-13 14:31:51 -0300

    feat: login on frontend

---
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../environment/environment';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.url.startsWith(environment.apiUrl)) {
+    req = req.clone({ withCredentials: true });
+  }
+
+  return next(req);
+};
--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,17 +1,43 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User, AuthRequest, AuthResponse } from '../models/auth.models';
+import { tap, catchError, Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  isAuthenticated = signal(false);
+  private readonly http = inject(HttpClient);
+  private readonly API_URL = environment.apiUrl;
 
-  login(credentials: any) {
-    console.log('AuthService communicating with API...', credentials);
-    this.isAuthenticated.set(true);
+  readonly currentUser = signal<User | null>(null);
+  readonly isLoading = signal<boolean>(false);
+
+  login(request: AuthRequest): Observable<AuthResponse> {
+    this.isLoading.set(true);
+
+    return this.http
+      .post<AuthResponse>(`${this.API_URL}/auth/login`, request, { withCredentials: true })
+      .pipe(
+        tap((res) => {
+          this.currentUser.set(res.user);
+          this.isLoading.set(false);
+        }),
+        catchError((err) => {
+          this.isLoading.set(false);
+          return throwError(() => err);
+        }),
+      );
   }
 
-  logout() {
-    this.isAuthenticated.set(false);
+  checkAuth(): Observable<User> {
+    return this.http.get<User>(`${this.API_URL}/auth/me`, { withCredentials: true }).pipe(
+      tap((user) => this.currentUser.set(user)),
+      catchError((err) => {
+        this.currentUser.set(null);
+        return throwError(() => err);
+      }),
+    );
   }
 }
